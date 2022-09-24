270c59f8cc4f689746cf19c0b57ebe3d9ecca2c1
Aadhikirubakaran <113748460+aadhikirubakaran@users.noreply.github.com>
2022-09-24 12:09:21 +0530

    Add files via upload

---
+++ Assessment MAIN FILE/M1 Aadhikirubakaran.K/assignment no2/tempurature detection/FINAL temp DETECTOR.py
@@ -0,0 +1,15 @@
+import random
+temp=list(range(0,100))
+for t in temp:
+        value=random.choice(temp)
+        if value>=43:
+           print(value);
+           print("TEMPURATURE IS HIGH!!!");
+else:
+    print("NORMAL");
+   
+    print(value);
+
+    print("HUMAN CAN SURVIVE BELOW 42.3 CELSIUS ONLY");
+    
+ 
