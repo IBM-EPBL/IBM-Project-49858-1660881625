f750fbf2b0b1e35f4f0c8a538730a823a3a92bed
Aadhikirubakaran <113748460+aadhikirubakaran@users.noreply.github.com>
2022-09-30 15:51:39 +0530

    Delete Assessment MAIN FILE/M1 Aadhikirubakaran.K/assignment no2/tempurature detection directory

--- Assessment MAIN FILE/M1 Aadhikirubakaran.K/assignment no2/tempurature detection/FINAL temp DETECTOR.py
+++
@@ -1,8 +0,0 @@
-import random
-temp=list(range(0,1000))
-for t in temp:
-        value=random.choice(temp)
-        if value>=43:
-           print("TEMPURATURE IS HIGH!!!",value);
-        else:
-            print("NORMAL",value);
